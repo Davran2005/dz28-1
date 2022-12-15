bcdf0bdec0ab6b43f259b9597a456238d4850c34
Davran2005 <davranjoldoshbaev2@gmail.com>
2022-12-15 15:01:57 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> list = Arrays.asList(5,23,6,0,12,123,6,5,1,99,3,4,1,2);
+        System.out.println("Сумма: "+list.stream().reduce(0, Integer::sum) );
+        IntSummaryStatistics a  = list.stream().mapToInt(Integer::intValue).summaryStatistics();
+        System.out.println("Арифметикалык орточо мааниси: "+Math.floor(a.getAverage() ) );
+        Set<Integer> items = new HashSet<>();
+        System.out.println("Окшош сандар: "+list.stream().filter(n -> !items.add(n) ).collect(Collectors.toSet() ) );
     }
 }
